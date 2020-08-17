10dea8cd33002aacdaded72141849974f7573ff4
Jessica Paczuski <jessica@figment.network>
2020-08-17 12:05:48 -0400

    Revert "Remove unused clients"
    This reverts commit 361c21016e8948b2e6432251949e0d886fa4daa5.

--- client/client.go
+++ client/client.go
@@ -13,22 +13,28 @@ func New(connStr string) (*Client, error) {
 	return &Client{
 		conn: conn,
 
-		Chain:       NewChainClient(conn),
-		Account:     NewAccountClient(conn),
-		Block:       NewBlockClient(conn),
-		Height:      NewHeightClient(conn),
-		Transaction: NewTransactionClient(conn),
+		Chain:                NewChainClient(conn),
+		Account:              NewAccountClient(conn),
+		Block:                NewBlockClient(conn),
+		Height:               NewHeightClient(conn),
+		Transaction:          NewTransactionClient(conn),
+		ValidatorPerformance: NewValidatorPerformanceClient(conn),
+		Staking:              NewStakingClient(conn),
+		Event:                NewEventClient(conn),
 	}, nil
 }
 
 type Client struct {
 	conn *grpc.ClientConn
 
-	Chain       ChainClient
-	Account     AccountClient
-	Block       BlockClient
-	Height      HeightClient
-	Transaction TransactionClient
+	Chain                ChainClient
+	Account              AccountClient
+	Block                BlockClient
+	Height               HeightClient
+	Transaction          TransactionClient
+	ValidatorPerformance ValidatorPerformanceClient
+	Staking              StakingClient
+	Event                EventClient
 }
 
 func (c *Client) Close() error {
